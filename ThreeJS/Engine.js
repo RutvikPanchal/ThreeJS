4b49e73ebbc9f61989b824ae7d016e9bea2fb95b
Rutvik Panchal <rutvikpanchal18@gmail.com>
2021-05-24 03:49:31 -0500

    Added the surface and Vertex slider, spherification has issues

--- ThreeJS/Engine.js
+++ ThreeJS/Engine.js
@@ -118,8 +118,9 @@ function orbitBottom(){
 
 }
 
+let holeFlag = true;
 function changeFactor(obj){
-    let factor = obj.value * 1;
+    let factor = obj.value;
     let vertices = sphere.geometry.attributes.position.array;
 
     for(let i = 0; i < vertices.length; i += 3){
@@ -130,6 +131,26 @@ function changeFactor(obj){
         vertices[i] = ((x * (1 - factor)) + factor * (2 * x / (1 + x * x + y * y)));
         vertices[i + 2] = ((y * (1 - factor)) + factor * (2 * y / (1 + x * x + y * y)));
         vertices[i + 1] = ((z * (1 - factor)) + factor * ((-1 + x * x + y * y) / (1 + x * x + y * y)));
+    }
+
+    if(obj.value > 0.995 && holeFlag){
+        sphereIndices.push(1, 2, 3);
+        sphereIndices.push(1, 2, 4);
+        sphereIndices.push(2, 5, 3);
+
+        sphere.geometry.setIndex(sphereIndices);
+        
+        holeFlag = false;
+    }
+
+    if(!holeFlag && obj.value < 0.995){
+        for(let i = 0; i < 9; i++){
+            sphereIndices.pop();
+        }
+
+        sphere.geometry.setIndex(sphereIndices);
+        
+        holeFlag = true;
     }
 
     sphere.geometry.attributes.position.needsUpdate = true;
@@ -144,21 +165,82 @@ function changeZoom(obj){
     zDelta = obj.value;
 }
 
+function chageVertSize(obj){
+    vertSize = obj.value;
+
+    let vertices = []
+    let normals = []
+    let colors = []
+    let indices = [];
+    let delaunayVerts = [];
+
+    for(let i = 0; i < vertSize; i++){
+
+        let dist = i / (vertSize - 1.0);
+        let azimuth = 2.0 * Math.PI * 1.61803398875 * i;
+        let inclination = Math.acos(1.0 - 2.0 * dist);
+    
+        let x = Math.sin(inclination) * Math.sin(azimuth);
+        let y = Math.sin(inclination) * Math.cos(azimuth);
+        let z = Math.cos(inclination);
+    
+        if(z == 1){
+            z = 0.99;
+        }
+    
+        vertices.push((x / (1.0 - z)), 0.0, (y / (1.0 - z)));
+        delaunayVerts.push((x / (1.0 - z)), (y / (1.0 - z)));
+        normals.push(0.0, 0.0, 1.0);
+        colors.push(0.3, 0.3, 0.3);
+    }
+
+    let factor = 1;
+
+    for(let i = 0; i < vertices.length; i += 3){
+        let x = vertices[i];
+        let z = vertices[i + 1];
+        let y = vertices[i + 2];
+
+        vertices[i] = ((x * (1 - factor)) + factor * (2 * x / (1 + x * x + y * y)));
+        vertices[i + 2] = ((y * (1 - factor)) + factor * (2 * y / (1 + x * x + y * y)));
+        vertices[i + 1] = ((z * (1 - factor)) + factor * ((-1 + x * x + y * y) / (1 + x * x + y * y)));
+    }
+
+    delaunay = new Delaunator(delaunayVerts);
+    for(let i = 0;i < delaunay.triangles.length; i++){
+        indices.push(delaunay.triangles[i]);
+    }
+
+    if(obj.value > 1){
+        indices.push(1, 2, 3);
+        indices.push(1, 2, 4);
+        indices.push(2, 5, 3);
+    }
+
+    sphere.geometry.setIndex(indices);
+    sphere.geometry.attributes.position.array = new Float32Array(vertices);
+    sphere.geometry.attributes.normal.array = new Float32Array(normals);
+    sphere.geometry.attributes.color.array = new Float32Array(colors);
+    
+    sphere.geometry.attributes.position.needsUpdate = true;
+    sphere.geometry.attributes.normal.needsUpdate = true;
+    sphere.geometry.attributes.color.needsUpdate = true;
+}
+
 // Materials
 const simpleMaterial = new THREE.MeshPhongMaterial({
     side: THREE.DoubleSide,
     vertexColors: true
 });
 
-const simpleTranslucentMaterial = new THREE.MeshPhongMaterial({
+const simpleTranslucentMaterial = new THREE.MeshNormalMaterial({
     side: THREE.DoubleSide,
-    vertexColors: true,
-    transparent: true,
-    opacity: 0.5,
+    flatShading: true,
 });
 
 const dotMaterial = new THREE.PointsMaterial({
-    size: 2,
+    size: 3,
+    color: 0x000000,
     sizeAttenuation: false
 });
 
@@ -168,13 +250,15 @@ const lineMaterial = new THREE.LineBasicMaterial({
 
 // Geometry Construction
 const sphereGeometry = new THREE.BufferGeometry();
-const sphereVertices = [];
-const sphereNormals = [];
-const sphereIndices = [];
-const sphereColors = [];
+var sphereVertices = [];
+var delaunayVertices = [];
+var sphereNormals = [];
+var sphereIndices = [];
+var sphereColors = [];
+
 
 // generate vertices
-vertSize = 1000
+var vertSize = 10000;
 
 for(let i = 0; i < vertSize; i++){
 
@@ -192,11 +276,18 @@ for(let i = 0; i < vertSize; i++){
 
     sphereVertices.push((x / (1.0 - z)), 0.0, (y / (1.0 - z)));
 
-    sphereColors.push(Math.random(), Math.random(), Math.random());
-    sphereNormals.push(0, 0, 0);
+    delaunayVertices.push((x / (1.0 - z)), (y / (1.0 - z)));
+
+    sphereColors.push(0.3, 0.3, 0.3);
+    sphereNormals.push(0.0, 0.0, 1.0);
 }
 
-//sphereGeometry.setIndex(sphereIndices);
+var delaunay = new Delaunator(delaunayVertices);
+for(let i = 0;i < delaunay.triangles.length; i++){
+    sphereIndices.push(delaunay.triangles[i]);
+}
+
+sphereGeometry.setIndex(sphereIndices);
 sphereGeometry.setAttribute('position', new THREE.Float32BufferAttribute(sphereVertices, 3));
 sphereGeometry.setAttribute('normal', new THREE.Float32BufferAttribute(sphereNormals, 3));
 sphereGeometry.setAttribute('color', new THREE.Float32BufferAttribute(sphereColors, 3));
@@ -231,7 +322,7 @@ const normals = [];
 const colors = [];
 
 // generate vertices
-vertices.push(0.0, 0.0, 0.0);
+vertices.push(0.0, 0.001, 0.0);
 
 vertices.push(0.5, 0.0, 0.0);
 vertices.push(0.0, 0.0, 0.5);
@@ -282,7 +373,8 @@ geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 const origin = new THREE.Line(originGeometry, lineMaterial);
 const mesh = new THREE.Mesh(geometry, simpleMaterial);
 const pointsCloud = new THREE.Points(geometry, dotMaterial);
-const sphere = new THREE.Points(sphereGeometry, dotMaterial);
+const sphere = new THREE.Mesh(sphereGeometry, simpleTranslucentMaterial);
+const spherePoints = new THREE.Points(sphereGeometry, dotMaterial);
 
 // Scene
 const scene = new THREE.Scene();
@@ -295,15 +387,53 @@ scene.add(light);
 scene.add(origin);
 scene.add(sphere);
 scene.add(pointsCloud);
+scene.add(spherePoints);
 
 // Code
     // Setup
-    camera.position.x = 1.5;
-    camera.position.y = 1.5;
-    camera.position.z = 2.2;
+    camera.position.x = 1;
+    camera.position.y = 2;
+    camera.position.z = 2;
     camera.lookAt(0, 0, 0);
 
     cameraControls.orbit(camera, pivot, 0.0, 0.0);
+
+    {
+        let factor = 1;
+        let vertices = sphere.geometry.attributes.position.array;
+
+        for(let i = 0; i < vertices.length; i += 3){
+            let x = sphereVertices[i];
+            let z = sphereVertices[i + 1];
+            let y = sphereVertices[i + 2];
+
+            vertices[i] = ((x * (1 - factor)) + factor * (2 * x / (1 + x * x + y * y)));
+            vertices[i + 2] = ((y * (1 - factor)) + factor * (2 * y / (1 + x * x + y * y)));
+            vertices[i + 1] = ((z * (1 - factor)) + factor * ((-1 + x * x + y * y) / (1 + x * x + y * y)));
+        }
+
+        if(factor > 0.995 && holeFlag){
+            sphereIndices.push(1, 2, 3);
+            sphereIndices.push(1, 2, 4);
+            sphereIndices.push(2, 5, 3);
+    
+            sphere.geometry.setIndex(sphereIndices);
+            
+            holeFlag = false;
+        }
+    
+        if(!holeFlag && factor < 0.995){
+            for(let i = 0; i < 9; i++){
+                sphereIndices.pop();
+            }
+    
+            sphere.geometry.setIndex(sphereIndices);
+            
+            holeFlag = true;
+        }
+
+        sphere.geometry.attributes.position.needsUpdate = true;
+    }
 
     document.getElementById("radius").innerText = cameraControls.getRadius(4);
     document.getElementById("inclination").innerText = cameraControls.getInclination(4);
